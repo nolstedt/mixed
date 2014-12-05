dd0183012e6add00d6f8fa9b5208d1700e038e6c
Martin Nolstedt <martin.nolstedt@lodon.se>
2014-12-05 18:30:47 +0100

    more sql

--- nolal/hw.rb
+++ nolal/hw.rb
@@ -16,8 +16,20 @@ where 1=1
 def get_current
   return "( #{DEF} ) current"
 end
-def get_current_extra(ggg)
-  return "( #{DEF} #{ggg} ) current"
+def get_current_extra(selections)
+  ands = ""
+  puts selections
+
+  selections.each do |selection|
+    #depending on the type !!
+    element = get_element(selection["name"])
+    onselect = element["onselect"]
+    ands = ands + onselect.gsub('REPLACE', selection['value'])
+    
+  end
+  current = "( #{DEF} #{ands} ) current"
+  puts current
+  return current
 end
 
 def get_element(name) 
@@ -25,14 +37,22 @@ def get_element(name)
   if name.eql?("gender") then
     out["name"] = "gender"
     out["type"] = "pie"
-    out["onselect"] = "and id in (select id from nolstedt where gender_c = 'VAL')"
-
+    out["select"] = ["gender_c"]
+    out["onselect"] = "and id in (select id from nolstedt where gender_c = 'REPLACE')"
 
   elsif name.eql?("teams") then
     out["name"] = "teams"
     out["type"] = "list"
-    out["onselect"] = "and id in (select id from nolstedt where team_id = 'VAL')"
+    out["select"] = ["team_id", "team_name", "team_id"]
+    out["onselect"] = "and id in (select id from nolstedt where team_id = 'REPLACE')"
+
+  elsif name.eql?("count") then
+    out["name"] = "count"
+    out["type"] = "current_count"
+    out["select"] = "count(*)"
+    #out["onselect"] = "and id in (select id from nolstedt where team_id = 'REPLACE')"
   end
+
   return out
 end
 
@@ -46,84 +66,87 @@ get '/element' do
   json out
 end
 
-get '/data' do 
-  client = Mysql2::Client.new(:host => "localhost", :username => "root", database: "sugarcrm")
-  name = params["name"]
-  out = get_element(name)
-
-  if out["type"].eql?("list") then
-    sel = out["onselect"]
-    val = params['VAL'] == nil ? "" : params["VAL"]
-  end
-
-  if val.length > 0 then
-    sel = sel.gsub('VAL', client.escape(val))
-    sql = "select team_id, team_name, count(team_id) team_id_c from #{get_current_extra(sel)} group by team_id"
-  else
-    sql = "select team_id, team_name, count(team_id) team_id_c from #{get_current} group by team_id"
-  end
+post '/data' do 
   
-  puts sql 
+  request.body.rewind  # in case someone already read it
+  body = request.body.read
+  data = JSON.parse body
 
-  results = client.query(sql)
-  out = {}
-  out["data"] = []
-  results.each do |r|
-    curr = []
-    curr << r["team_id"]
-    curr << r["team_name"]
-    curr << r["team_id_c"]
-    out["data"] << curr
+  client = Mysql2::Client.new(:host => "localhost", :username => "root", database: "sugarcrm")
+  toupdate_name = data["toupdate"]
+  toupdate = get_element(toupdate_name)
+  
+
+  if toupdate["type"].eql?("list") then
+    if data["selection"].nil? then
+      sql = "select #{toupdate["select"][0]}, #{toupdate["select"][1]}, count(#{toupdate["select"][2]}) from #{get_current} group by #{toupdate["select"][2]}" 
+    else
+      current = get_current_extra(data["selection"])
+      sql = "select #{toupdate["select"][0]}, #{toupdate["select"][1]}, count(#{toupdate["select"][2]}) from #{current} group by #{toupdate["select"][2]}"  
+    end
+
+    results = client.query(sql)
+    out = {}
+    out["data"] = []
+    results.each(:as => :array) do |row|
+      curr = []
+      curr << row[0]
+      curr << row[1]
+      curr << row[2]
+      out["data"] << curr
+    end
+
+  elsif toupdate["type"].eql?("pie") then
+    if data["selection"].nil? then
+      sql_c = "select count(*) from #{get_current}"
+      sql = "select #{toupdate["select"][0]}, count(#{toupdate["select"][0]}) from #{get_current} group by #{toupdate["select"][0]}" 
+    else
+      current = get_current_extra(data["selection"])
+      sql_c = "select count(*) from #{current}"
+      sql = "select #{toupdate["select"][0]}, count(#{toupdate["select"][0]}) from #{current} group by #{toupdate["select"][0]}" 
+    end
+    
+    results_c = client.query(sql_c)
+    results = client.query(sql)
+
+    count = 0
+    results_c.each(:as => :array) do |row|
+      count = row[0]
+    end
+
+    out = {}
+    out["data"] = []
+    results.each(:as => :array) do |row|
+      curr = []
+      curr << row[0]
+      curr << row[1]
+      curr << (row[1].to_f / count)
+      out["data"] << curr
+    end
+  
+  elsif toupdate["type"].eql?("current_count") then
+
+    if data["selection"].nil? then
+      current = get_current
+    else
+      current = get_current_extra(data["selection"])
+    end
+    sql = "select #{toupdate["select"]} from #{current}"
+    
+    results = client.query(sql)
+    count = 0
+    results.each(:as => :array) do |row|
+      count = row[0]
+    end
+    out = {}
+    out["data"] = {}
+    out["data"]["count"] = count
   end
+    
+  
   client.close
   json out
 
-end
-
-get '/teams' do
-  
-  #apply where part...
-
-end
-
-get '/gender' do
-  client = Mysql2::Client.new(:host => "localhost", :username => "root", database: "sugarcrm")
-  sel = params['sel'] == nil ? "" : params["sel"]
-  val = params['VAL'] == nil ? "" : params["VAL"]
-
-puts sel 
-puts val
-puts val.class
-
-  #apply where part...
-  if sel.length > 0 then
-    sel = sel.gsub('VAL', client.escape(val))
-    sql_c = "select count(*) as c from #{get_current_extra(sel)}"
-    sql = "select gender_c, count(gender_c) gender_c_count from #{get_current_extra(sel)} group by gender_c"
-  else
-    sql_c = "select count(*) as c from #{get_current}"
-    sql = "select gender_c, count(gender_c) gender_c_count from #{get_current} group by gender_c"
-  end
-
-  puts sql
-  puts sql_c
-  
-  results_c = client.query(sql_c)
-
-  results = client.query(sql)
-
-  c = results_c.first["c"]
-  out = {}
-  out["data"] = []
-  results.each do |r|
-    curr = []
-    curr << r["gender_c"]
-    curr << r["gender_c_count"]
-    curr << (r["gender_c_count"].to_f / c)
-    out["data"] << curr
-  end
-  client.close
-  json out
 end
 
 
