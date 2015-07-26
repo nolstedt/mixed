4cb7b75685389fdb22bf5365f9cbd9ba7745a2b2
nolstedt <martin@nolstedt.se>
2015-07-26 09:11:47 +0000

    update sensor tester

--- 1wire_check/check_tempsensors.rb
+++ 1wire_check/check_tempsensors.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/ruby
 
-sens = {}
-sens["28.AB46E8030000"] = "chill"
-sens["28.DD63E8030000"] = "bastu"
-
 f = "/mnt/1wire"
 Dir.chdir f
 t = Dir.glob "28*"
-
 t.each do |x| 
 	a=File.join(f, x, "temperature")
-	puts "#{a} #{sens[x]}  #{File.read(a)}"
+	puts "#{a} #{File.read(a)}"
+end
+
+t = Dir.glob "26*"
+t.each do |x| 
+	a=File.join(f, x, "humidity")
+	puts "#{a} #{File.read(a)}"
 end
